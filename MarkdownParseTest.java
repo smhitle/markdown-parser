85e8d9207f2cabefbd810b7c49cbca94af7bbb17
smhitle <91502030+smhitle@users.noreply.github.com>
2022-05-05 09:09:44 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,13 +1,14 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-import java.nio.file.Path;
+import java.nio.file.Path.*;
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
 
@@ -19,14 +20,11 @@ public class MarkdownParseTest {
     @Test
     public void testGetLinks() throws IOException{
         List<String> list = List.of("https://something.com", "some-thing.html");
-        try {
-            Path fileName = Path.of("test-file.md"); 
-            String content = Files.readString(fileName);
-            ArrayList<String> links = MarkdownParse.getLinks(content);
-            assertEquals(list, links);
-        } catch (NoSuchFileException e) {
-            
-        }
+        Path fileName = Path.of("C:/Users/lienm/Documents/GitHub/markdown-parser/test-file.md"); 
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        assertEquals(list, links);
     }
 }
+
 
