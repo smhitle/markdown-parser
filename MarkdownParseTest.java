bec31b8fa91e02609673a33323544f4bf4fa2a1f
smhitle <91502030+smhitle@users.noreply.github.com>
2022-05-08 23:16:26 -0700

    a

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,14 +1,12 @@
 import static org.junit.Assert.*;
-import org.junit.*;
-
-import java.nio.file.Path.*;
-import java.util.*;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
+import java.nio.file.;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
 
 public class MarkdownParseTest {
 
@@ -19,10 +17,10 @@ public class MarkdownParseTest {
 
     @Test
     public void testGetLinks() throws IOException{
-        List<String> list = List.of("https://something.com","some-thing.html");
-        Path fileName = Path.of("/Users/lienm/Documents/GitHub/markdown-parser/test-file.md");
+        Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
+        List<String> links = MarkdownParse.getLinks(content);
+        List<String> list = List.of("https://something.com","some-thing.html");
         assertEquals(list, links);
     }
 }
