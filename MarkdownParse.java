8eb7d5d15ed1bab612c456dd0b0988fe4c1340df
Tyler Le <cs15lsp22awq@ieng6-201.ucsd.edu>
2022-05-14 16:48:12 -0700

    added comment line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
+	// added new line
         ArrayList<String> toReturn = new ArrayList<>();
         int currentIndex = 0;
         String[] fileSubstrings = markdown.split("\n");
