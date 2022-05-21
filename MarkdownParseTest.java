d393d10ac7d74c721bf0a16d07c21e08d12bcbf5
smhitle <91502030+smhitle@users.noreply.github.com>
2022-05-21 16:06:01 -0700

    lab 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -22,6 +22,34 @@ public class MarkdownParseTest {
         List<String> list = List.of("https://something.com","some-thing.html");
         assertEquals(list, links);
     }
+
+    @Test
+    public void snippet1GetLinks() throws IOException {
+        Path fileName = Path.of("snippet1.md");
+        String content = Files.readString(fileName);
+        List<String> links = MarkdownParse.getLinks(content);
+        List<String> list = List.of("`google.com","google.com", "ucsd.edu");
+        assertEquals(list, links);
+    }
+
+    @Test
+    public void snippet2GetLinks() throws IOException {
+        Path fileName = Path.of("snippet2.md");
+        String content = Files.readString(fileName);
+        List<String> links = MarkdownParse.getLinks(content);
+        List<String> list = List.of("a.com","a.com(())", "example.com");
+        assertEquals(list, links);
+    }
+
+    @Test
+    public void snippet3GetLinks() throws IOException {
+        Path fileName = Path.of("snippet3.md");
+        String content = Files.readString(fileName);
+        List<String> links = MarkdownParse.getLinks(content);
+        List<String> list = List.of("https://www.twitter.com","https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", "https://cse.ucsd.edu/");
+        assertEquals(list, links);
+    }
+
 }
 
 
