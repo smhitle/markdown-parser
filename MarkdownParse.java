e0faefab07945663df43fea683d76587c191c322
smhitle <91502030+smhitle@users.noreply.github.com>
2022-05-21 00:16:47 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
-    
+
     public static ArrayList<String> getLinks(String markdown) {
 	// added new line
         ArrayList<String> toReturn = new ArrayList<>();
